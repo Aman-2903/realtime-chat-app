8c76605214b8fa230726abc180922e58f0765403
aman <kushwaha@gmail.com>
2024-07-07 20:52:28 +0530

    create express server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+http.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
